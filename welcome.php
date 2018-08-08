7c57d09534530cccf96a50667f1a82967878e73e
AnjithaJohnson <39301324+AnjithaJohnson@users.noreply.github.com>
2018-08-08 15:39:06 +0530

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -20,7 +20,7 @@ alert("assssss");
   $test = "0";
   // Data for text message. This is the text message data.
   $sender = "Lifestream"; // This is who the message appears to be from.
-  $numbers = "$to"; // A single number or a comma-seperated list of numbers
+  $numbers = $to; // A single number or a comma-seperated list of numbers
   $message = "Welcome to Our Website LifeStream.";
   // 612 chars or less
   // A single number or a comma-seperated list of numbers
